a80a0054d56e799e3fd529fe90d27405d34e475a
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-08-02 17:59:17 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ app.get("/message/:id/:name", (request, response) => {// ':id/:name' são inform
     response.send(`Mensagem ID: ${id}. Para ${name}`)
 })
 
+app.get("/users", (request, response) => {// com o Query Params os elementos são passados na URL desse jeito 'servidor/users?page=2&limit=30'
+    const { page, limit } = request.query//pegando os paramentros 'page' e 'limit'
+
+    response.send(`Página: ${page}. Limite: ${limit}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
