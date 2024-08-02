0f9b33b1dcf3f40b4f47b414fb8aed5c910c2e8d
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-08-02 17:25:15 -0300

    Método GET

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express()
 
+app.get("/message", (request, response) => {
+    response.send("Hello, world!")
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
