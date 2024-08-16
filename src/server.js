daf996c8b69e53780ea5ed022c58c7c7d14a9915
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-08-16 15:13:08 -0300

    POST methode

--- src/server.js
+++ src/server.js
@@ -2,16 +2,9 @@ const express = require("express")
 
 const app = express()
 
-app.get("/message/:id/:name", (request, response) => {// ':id/:name' são informações passadas para o GET atraves da URL
-    const { id, name } = request.params//pegando os paramentros 'id' e 'name'
+app.post("/users", (request, response) => {
 
-    response.send(`Mensagem ID: ${id}. Para ${name}`)
-})
-
-app.get("/users", (request, response) => {// com o Query Params os elementos são passados na URL desse jeito 'servidor/users?page=2&limit=30'
-    const { page, limit } = request.query//pegando os paramentros 'page' e 'limit'
-
-    response.send(`Página: ${page}. Limite: ${limit}`)
+    response.send("Você está no metodo POST")
 })
 
 const PORT = 3333
